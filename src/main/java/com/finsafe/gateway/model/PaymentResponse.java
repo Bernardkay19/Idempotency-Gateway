6f0e1893c7bbc8567c91b70f9ae2c7c844734696
Bernard Kwaku Duah <kwakuduahbernard136@gmail.com>
2026-06-26 02:18:54 +0000

    feat: define payment request and response dtos

---
+++ src/main/java/com/finsafe/gateway/model/PaymentResponse.java
@@ -0,0 +1,14 @@
+package com.finsafe.gateway.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentResponse {
+    private String status;
+}
